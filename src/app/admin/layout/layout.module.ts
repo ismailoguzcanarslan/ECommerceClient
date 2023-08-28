8898766c6841620c0f55e5bede3e15cf185e2196
ismailoguzcanarslan <ismailoguzcanarslan@gmail.com>
2023-08-29 01:51:46 +0300

    multiple layout infrastructure

--- src/app/admin/layout/layout.module.ts
+++ src/app/admin/layout/layout.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { ComponentsModule } from './components/components.module';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 
 
@@ -11,7 +12,8 @@ import { ComponentsModule } from './components/components.module';
   ],
   imports: [
     CommonModule,
-    ComponentsModule
+    ComponentsModule,
+    AppRoutingModule,
   ],
   exports:[
     LayoutComponent
