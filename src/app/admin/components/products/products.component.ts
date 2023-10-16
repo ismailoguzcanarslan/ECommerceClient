b5835c1ebde36428b167b765f88f5fc554424aed
ismailoguzcanarslan <ismailoguzcanarslan@gmail.com>
2023-10-16 23:22:46 +0300

    add and list product

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -4,22 +4,29 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { Product } from 'src/app/contracts/product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
-
   constructor(spinner: NgxSpinnerService, private httpClientService : HttpClientService) {
     super(spinner);
   }
 
   ngOnInit(): void{
     this.showSpinner(SpinnerType.BallClipRotatePulse);
-    this.httpClientService.get<Product[]>({
-      controller: "products"
-    }).subscribe(data => console.log(data));
+    
+    // this.httpClientService.get<Product[]>({
+    //   controller: "products"
+    // }).subscribe(data => console.log(data));
 
     // this.httpClientService.put({
     //   controller: "products"
@@ -33,10 +40,10 @@ export class ProductsComponent extends BaseComponent implements OnInit {
     // },
     // "ccfe34a2-52ce-4d26-9413-ff6823e5d3bb").subscribe();
 
-    this.httpClientService.get(
-      {
-        baseUrl: "https://jsonplaceholder.typicode.com",
-        controller: "posts"
-      }).subscribe(data=> console.log(data));
+    // this.httpClientService.get(
+    //   {
+    //     baseUrl: "https://jsonplaceholder.typicode.com",
+    //     controller: "posts"
+    //   }).subscribe(data=> console.log(data));
   }
 }
